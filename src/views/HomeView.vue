ffea895fe3c5ee9623392201c9e04caa14e370aa
LucasPmep <lucas.gillet@pme-partner.fr>
2023-04-18 11:09:07 +0200

    lesson 6 : classic error handling with vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,10 +11,14 @@
         <ul 
         v-if="mapboxSearchResults"
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
-          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id"
-          class="py-2 cursor-pointer">
-        {{ searchResult.place_name }}
-        </li>
+          <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+          <p v-if="!searchError && mapboxSearchResults.length === 0">No results macth your query, try a different term.</p>
+          <template v-else>
+            <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id"
+            class="py-2 cursor-pointer">
+              {{ searchResult.place_name }}
+            </li>
+          </template>
         </ul>
     </div>
   </main>
@@ -28,14 +32,20 @@ const mapboxAPIKey = "pk.eyJ1IjoibHVjYXNwbWVwIiwiYSI6ImNsZ20wazh0bTAxN3IzZW8yZ3J
 const searchQuery = ref("")
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}`);
-      mapboxSearchResults.value = result.data.features;
-      console.log(mapboxSearchResults.value);
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}`);
+        mapboxSearchResults.value = result.data.features;
+        console.log(mapboxSearchResults.value);
+      } catch {
+        searchError.value = true;
+      }
+
       return;
     }
     mapboxSearchResults.value = null;
